49c0fb514fbb4abcf85be27f75a4ffdfe75d4b24
unaay20 <junml.kimmich@gmail.com>
2025-03-03 12:44:43 -0600

    Agregué el controlador de fotos

--- src/server.js
+++ src/server.js
@@ -12,13 +12,13 @@ app.use("/uploads", express.static("uploads")); //Servir imagenes estaticamente
 mongoose
     .connect(process.env.MONGO_URI, {
         userNewUrlParser: true,
-        useUnifiedTopology: true
+        userUnifiedTopology: true
     })
     .then(() => console.log("MongoDB conectado"))
     .catch((err)=> console.error("Error de conexión ", err));
 
 //Importar rutas
 const photoRoutes = require("./routes/photoRoutes");
-app.use("api/photos", photoRoutes);
+app.use("/api/photos", photoRoutes);
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, ()=> console.log('Servidor corriendo en el puerto ${PORT}'));
