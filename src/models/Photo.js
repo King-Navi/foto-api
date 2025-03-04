79f5de0952a85f4bad32d29b05099faa565a4c07
mauricinio7 <miory.salas@hotmail.com>
2025-03-04 17:00:38 -0600

    Some bugs has Fixed

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -1,8 +1,7 @@
 const mongoose = require("mongoose");
 const PhotoSchema = new mongoose.Schema({
-    userId: { type:String, required:true}, //Usuario porpietario de la foto
-    imageUrl: { type:String, required:true}, //URL de la imagen subida
-    createdAt: { type:Date, default:Date.now} //Fecha de creación
+userId: { type: String, required: true }, // Usuario propietario de la foto
+imageUrl: { type: String, required: true }, // URL de la imagen subida
+createdAt: { type: Date, default: Date.now },
 });
-
 module.exports = mongoose.model("Photo", PhotoSchema);
