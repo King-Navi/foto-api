1efa765c4bb770651d65cf03a833af9d4d7ab3e4
unaay20 <junml.kimmich@gmail.com>
2025-03-07 12:38:00 -0600

    Server terminado, se agregaron pruebas de las 3 solicitudes

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
-require("dotenv").config();
+require("dotenv").config({ path: require("path").resolve(__dirname, "../.env") });
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use("/uploads", express.static("uploads")); // Servir imágenes estáticamente
 
-/* Conexión a MongoDB */
+
+app.use("/uploads", express.static("uploads")); 
+
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => console.log("MongoDB conectado"))
-    .catch((err) => console.error("Error de conexión", err));
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB conectado"))
+  .catch((err) => console.error("Error de conexión:", err));
 
-// Importar rutas
 const photoRoutes = require("./routes/photoRoutes");
 app.use("/api/photos", photoRoutes);
 
-const PORT = process.env.PORT || 5001;
-app.listen(PORT, () => console.log(`Servidor corriendo en el puerto ${PORT}`));
+module.exports = app; 
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+}
